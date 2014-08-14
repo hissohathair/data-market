2aa1f27950190b5f3c08d926b78b7c9dffda3751
Sören Balko <Soeren.Balko@gmail.com>
2014-08-15 09:50:37 +1000

    Dummy roundtrip

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,13 @@
+var timelineAPI = require('../lib/api/timeline')
 
-/*
- * GET home page.
- */
+exports.init = function(app) {
+	app.get('/', function(req, res){
+	  res.render('index', { title: 'Express' });
+	});	
 
-exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+	app.get('/timeline', function(req, res) {
+		timelineAPI.getTimeline(req.query.term, function(responsePayload) {			
+			res.json(responsePayload);
+		});
+	});
 };
